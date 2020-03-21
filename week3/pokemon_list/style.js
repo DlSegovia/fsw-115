9537d7be649695cde5de85c9367d17c5c913514b
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-03-20 20:58:08 -0500

    late assignment week3

--- week3/pokemon_list/style.js
+++ week3/pokemon_list/style.js
@@ -1,19 +1,27 @@
-const xhr = new XMLHttpRequest()
-    // method  // url                     // async?
-xhr.open("GET", "https://swapi.co/api/people/1", true)
-xhr.send()
+function loadXML() {
+    //create new xhr object by initializing XMLHTTPREQUEST
+    const xhr = new XMLHttpRequest();
+    //define our CRUD operation
+    xhr.open("GET", "https://api.vschool.io/pokemon", true);
+    //similar to a function needs to be "called" or actually sent
+    xhr.send();
+    //interact with our recieved data
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr.responseText)
+            const jsonData = xhr.responseText;
+            const data = JSON.parse(jsonData);
+            showData(data.objects[0].pokemon)
+        }
+    };
 
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText)
+    function showData(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            const h1 = document.createElement('h1')
+            h1.innerHTML = "name: " + arr[i].name + "</br> resource_uri: " + arr[i].resource_uri
+                // h1.textContent = "name: " + arr[i].name ", resource_uri: " + arr[i].resource_uri
+            document.body.append(h1)
+        }
     }
 }
-
-
-// function showData(arr) {
-//     for (let i = 0; i < arr.length; i++) {
-//         const h1 = document.createElement('h1')
-//         h1.textContent = arr[i].name
-//         document.body.appendChild(h1)
-//     }
-// }
+loadXML()
