9e12c75e2f24ec300f5d07e45ec2bd3b7f7881a2
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-03-20 21:09:28 -0500

    Update style.js
    removed un-needed commits.

--- week3/pokemon_list/style.js
+++ week3/pokemon_list/style.js
@@ -1,11 +1,8 @@
 function loadXML() {
-    //create new xhr object by initializing XMLHTTPREQUEST
+    
     const xhr = new XMLHttpRequest();
-    //define our CRUD operation
     xhr.open("GET", "https://api.vschool.io/pokemon", true);
-    //similar to a function needs to be "called" or actually sent
     xhr.send();
-    //interact with our recieved data
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {
             console.log(xhr.responseText)
@@ -19,7 +16,6 @@ function loadXML() {
         for (let i = 0; i < arr.length; i++) {
             const h1 = document.createElement('h1')
             h1.innerHTML = "name: " + arr[i].name + "</br> resource_uri: " + arr[i].resource_uri
-                // h1.textContent = "name: " + arr[i].name ", resource_uri: " + arr[i].resource_uri
             document.body.append(h1)
         }
     }
