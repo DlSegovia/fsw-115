7364ab3b142b496383044f6de7bdb06ca9f59736
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-04-08 23:49:00 -0500

    project 2 week4

--- week4/project_2_Cceate_a_website_with_multiple_requests/index.js
+++ week4/project_2_Cceate_a_website_with_multiple_requests/index.js
@@ -7,15 +7,19 @@ btn1.addEventListener("click", function() {
             for (let i = 0; i < response.data.length; i++) {
                 const div = document.createElement('div')
                 document.body.append(div)
+
                 const h1 = document.createElement('h1')
                 h1.textContent = response.data[i].title
                 div.appendChild(h1);
+
                 const h3 = document.createElement('h3')
                 h3.textContent = response.data[i].description
                 div.appendChild(h3);
+
                 const h4 = document.createElement('h4')
                 h4.textContent = response.data[i].price
                 div.appendChild(h4);
+
                 const p = document.createElement('p')
                 p.textContent = response.data[i].completed
                 document.body.append(p);
