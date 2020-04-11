a462edce5dfe02dcaa1ccd28d1cd2254ea13901d
Daniel Segovia <58751893+DlSegovia@users.noreply.github.com>
2020-04-10 20:34:56 -0500

    update for project 1

--- week2/create_a_stylized_list/index.js
+++ week2/create_a_stylized_list/index.js
@@ -1,23 +1,44 @@
-var newH1 = document.createElement("h1")
-newH1.textContent = "Name: Chewbacca"
-document.body.append(newH1)
+// var newH1 = document.createElement("h1")
+// newH1.textContent = "Pokemon List"
+// document.body.append(newH1)
 
-var newUl = document.createElement("ul")
-newUl.id = "myList"
-document.body.append(newUl)
+var newp = {
+    "pokemon": [
 
-const names = [
-    "height: 228",
-    "mass: 112",
-    "hair_color: brown",
-    "skin_color: unknown",
-    "eye_color: blue",
-    "birth_year: 200BBY",
-    "gender: male"
-]
-
-for (let i = 0; i < names.length; i++) {
-    var newLi = document.createElement("li")
-    newLi.textContent = names[i]
-    myList.append(newLi)
+        {
+            "name": "rattata",
+            "resource_uri": "api/v1/pokemon/19/"
+        },
+        {
+            "name": "charmander",
+            "resource_uri": "api/v1/pokemon/4/"
+        },
+        {
+            "name": "charmeleon",
+            "resource_uri": "api/v1/pokemon/5/"
+        },
+        {
+            "name": "wartortle",
+            "resource_uri": "api/v1/pokemon/8/"
+        }
+    ]
 }
+
+// var newUl = document.createElement("ul")
+// newUl.id = "pokemons"
+// document.body.append(newUl)
+
+var obj = JSON.stringify(newp)
+document.body.innerHTML = obj
+
+
+// var pokemonList = document.getElementById("pokemons")
+
+// for (let i = 0; i < newp.length; i++) {
+//     var newName = document.createElement("li")
+//     newName.textContent = newp[i]
+//     newp.append(newName)
+// }
+// var newH1 = document.createElement("h1")
+// newH1.textContent = "Pokemon"
+// document.body.append(newH1)
